5b8d7df2ee11f460dafa073a86a1ccf632157a28
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-01-21 16:08:10 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -6,7 +6,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: { lat: 35.717, lng: 139.731 },
+    center: { lat:-25.786416802936397, lng: 134.39842121934447 }, 
   });
 }
 
