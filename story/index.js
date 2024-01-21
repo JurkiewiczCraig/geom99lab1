59792dfa18f0bdd914a9ed817790d5e0d7df2e92
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-01-21 16:00:09 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,13 +1,13 @@
-let map: google.maps.Map;
-async function initMap(): Promise<void> {
-  const { Map } = (await google.maps.importLibrary(
-    "maps",
-  )) as google.maps.MapsLibrary;
-  map = new Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
+// This example displays a map with the language and region set
+// to Japan. These settings are specified in the HTML script element
+// when loading the Google Maps JavaScript API.
+// Setting the language shows the map in the language of your choice.
+// Setting the region biases the geocoding results to that region.
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
+    center: { lat: 35.717, lng: 139.731 },
   });
 }
 
-initMap();
-export {};
+window.initMap = initMap;
