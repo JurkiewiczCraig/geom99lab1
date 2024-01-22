17793d7e26f4f450f48bb7298813c91ea96d1174
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-01-22 12:23:44 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,51 +1,12 @@
-async function initMap() {
-  // Request needed libraries.
-  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-    "marker",
-  );
-  const map = new Map(document.getElementById("map"), {
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+function initMap() {
+  const uluru = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: { lat: -25.792046299019553, lng: 132.3348778563882 },    
-    mapId: "4504f8b37365c3d0",
+    center: uluru,
   });
-  // Set LatLng and title text for the markers. The first marker (Boynton Pass)
-  // receives the initial focus when tab is pressed. Use arrow keys to
-  // move between markers; press tab again to cycle through the map controls.
-  const tourStops = [
-  
-    {
-      position: { lat: -26.678056, lng: 153.118333 },
-      title: "Steve Irwin Monument",
-    },
-    {
-      position: { lat: -16.416235940808605, lng: 145.771535726202 },    
-      title: "Batt Reef",
-    },
-    {
-      position: { lat: -37.89838263906072, lng: 145.31301015076852 },    
-      title: "Upper FernTree Gully",
-    },
-    {
-      position: { lat: -12.37533068709093,  lng: 142.17720449182988 },   
-      title: "Steve Irwin Wildlife Reserve",
-    },
-  ];
-  // Create an info window to share between markers.
-  const infoWindow = new InfoWindow();
-
-  // Create the markers.
-  tourStops.forEach(({ position, title }, i) => {
-    const pin = new PinElement({
-      glyph: `${i + 1}`,
-    });
-    const marker = new AdvancedMarkerElement({
-      position,
-      map,
-      title: `${i + 1}. ${title}`,
-      content: pin.element,
-    });
-const contentString1 =
+  const contentString =
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
@@ -66,28 +27,20 @@ const contentString1 =
     "(last visited June 22, 2009).</p>" +
     "</div>" +
     "</div>";
-  const infowindow1 = new google.maps.InfoWindow({
-    content: contentString1,
-    ariaLabel: "Australia Zoo",
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Uluru",
   });
-  const marker1 = new google.maps.Marker({
-    position: { lat: -26.835804237163565, lng: 152.96313445196486 },
+  const marker = new google.maps.Marker({
+    position: uluru,
     map,
-    title: "Australia Zoo",
+    title: "Uluru (Ayers Rock)",
   });
 
-    
-    // Add a click listener for each marker, and set up the info window.
-    marker.addListener("click", ({ domEvent, latLng }) => {
-      const { target } = domEvent;
-
-      infoWindow.close();
-      infoWindow.setContent(marker.title);
-      infoWindow.open(marker.map, marker);
-   infowindow1.close();
-      infowindow1.open(marker.map, marker);
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
     });
   });
 }
-
-initMap();
