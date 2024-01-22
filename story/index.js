f39877bf3ab33eff1e55d0eead41298557b9aee3
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-01-22 18:44:01 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -29,6 +29,45 @@ async function initMap() {
     content: faPin.element,
     title: "Steve Irwin Monument",
   });
+  const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Monument",
+  });
+  const marker = new google.maps.Marker({
+    position: { lat:-26.838493, lng: 152.959481}, 
+    map,
+    title: "Steve Irwin Monument",
+  });
+
+ marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  });
+
+  
 // a Marker for Steve Irwin's home town
   const city = document.createElement("div");
 
