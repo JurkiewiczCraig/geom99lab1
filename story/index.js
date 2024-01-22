8f21560e8bbd0bf2a5cad7b235511e4c309d3847
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-01-21 19:15:30 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,17 +1,68 @@
+async function initMap() {
+  // Request needed libraries.
+  const { Map, InfoWindow } = (await google.maps.importLibrary(
+    "maps",
+  )) as google.maps.MapsLibrary;
+  const { AdvancedMarkerElement, PinElement } =
+    (await google.maps.importLibrary("marker")) as google.maps.MarkerLibrary;
 
-function initMap() {
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: { lat: -26.16226621694845, lng:  134.50294137713874 },  
+  const map = new Map(document.getElementById("map") as HTMLElement, {
+    zoom: 12,
+    center: { lat: 34.84555, lng: -111.8035 },
+    mapId: "4504f8b37365c3d0",
+  });
+
+  // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+  // receives the initial focus when tab is pressed. Use arrow keys to
+  // move between markers; press tab again to cycle through the map controls.
+  const tourStops = [
+    {
+      position: { lat: 34.8791806, lng: -111.8265049 },
+      title: "Boynton Pass",
+    },
+    {
+      position: { lat: 34.8559195, lng: -111.7988186 },
+      title: "Airport Mesa",
+    },
+    {
+      position: { lat: 34.832149, lng: -111.7695277 },
+      title: "Chapel of the Holy Cross",
+    },
+    {
+      position: { lat: 34.823736, lng: -111.8001857 },
+      title: "Red Rock Crossing",
+    },
+    {
+      position: { lat: 34.800326, lng: -111.7665047 },
+      title: "Bell Rock",
+    },
+  ];
+
+  // Create an info window to share between markers.
+  const infoWindow = new InfoWindow();
+
+  // Create the markers.
+  tourStops.forEach(({ position, title }, i) => {
+    const pin = new PinElement({
+      glyph: `${i + 1}`,
+    });
+
+    const marker = new AdvancedMarkerElement({
+      position,
+      map,
+      title: `${i + 1}. ${title}`,
+      content: pin.element,
+    });
+
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", ({ domEvent, latLng }) => {
+      const { target } = domEvent;
+      infoWindow.close();
+      infoWindow.setContent(marker.title);
+      infoWindow.open(marker.map, marker);
+    });
   });
 }
 
-  const marker = new AdvancedMarkerElement({
-    map,
-    position: { lat: 37.4239163, lng: -122.0947209 },
-  });
-
-
-
-window.initMap = initMap;
+initMap();
+export {};
