ccfbc499264c3489e5c2ac3de53213a9130f52c6
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-01-22 22:28:48 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -84,19 +84,18 @@ async function initMap() {
     "</div>" +
     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "<p><b>Upper Fern Tree Gully</b> was Steve Irwin's Hometown, he was born on February " +
+    "22nd 1962 to Robert and Lyn Hakainsson.  Both parents were avid naturalists " +
+    "and decided to open up their own wildlife park where Steve quickly took a liking" +
+    "to and spent most of his free time spending time with the animals, paying particular"+
+    " close attention to the crocodiles.  He is reported to have wrestled his first crocodile" +
+    "at the tender age of nine (while under his fathers supervision) " +
     "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
     "Aboriginal people of the area. It has many springs, waterholes, " +
     "rock caves and ancient paintings. Uluru is listed as a World " +
     "Heritage Site.</p>" +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-    "(last visited June 22, 2009).</p>" +
+    '<p>Attribution: Steve Irwin's early life, <a href="https://en.wikipedia.org/wiki/Steve_Irwin">' +
+    </p>" +
     "</div>" +
     "</div>";
   const infowindow1 = new google.maps.InfoWindow({
