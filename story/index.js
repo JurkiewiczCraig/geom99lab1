33d58dd9afdfee00e119a4bbbc74ebdec6ab3c37
JurkiewiczCraig <146446974+JurkiewiczCraig@users.noreply.github.com>
2024-01-22 20:02:39 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -33,19 +33,12 @@ async function initMap() {
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<h1 id="firstHeading" class="firstHeading">Monument To Steve Irwin</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Heritage Site.</p>" +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "<p>A monument to Steve Irwin was built here in 2007, built to recognize Steve Irwin's dedication to conservation, education and his family.  The statue 
+    depicts Irwin, with his two children, Bindi and Robert "while" holding a crocodile.  The monument is located right by the beach in 
+    Mooloolaba </p>" +
+    '<p>Attribution: Mooloolaba , <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
     "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
     "(last visited June 22, 2009).</p>" +
     "</div>" +
